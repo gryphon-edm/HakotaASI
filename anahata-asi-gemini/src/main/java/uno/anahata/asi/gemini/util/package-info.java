741dc04fe9dcd29094c888b9093b117bbb89d25f
priyanahata <p@anahata.uno>
2026-04-03 22:11:03 +0200

    bye bye

--- anahata-asi-gemini/src/main/java/uno/anahata/asi/gemini/util/package-info.java
+++
@@ -1,7 +0,0 @@
-/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
-/**
- * Provides utility classes specific to the Gemini provider.
- *
- * @author anahata-gemini-pro-2.5
- */
-package uno.anahata.asi.gemini.util;
